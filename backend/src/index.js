85330ee6356807b0bc7473f8e5bec2d53dd2cb42
POOVARASAN-03 <poovarasang1212@gmail.com>
2025-09-30 14:29:35 +0530

    Updated index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,7 @@ import { app, server } from "../lib/socket.js";
 import path from "path";
 
 dotenv.config();
+const __dirname = path.resolve();
 // Enable CORS first
 app.use(cors({
     origin: "http://localhost:5173",
@@ -30,8 +31,8 @@ app.use('/api/messages', messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
